4b581b6f44cf96d7d4c85f651cec33d64fb46853
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-07 14:21:25 +0200

    .

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,88 +1,90 @@
+// importing calendar/date plugin
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-
-
+// importing pop-ups (toasting) plugin
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 
-const currentTime = new Date();
-const userInput = document.querySelector('#datetime-picker');
-let UserSelectedDate = null;
-const startBtn = document.querySelector('.js-btn')
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    if (selectedDates[0] < currentTime) {
-      iziToast.error({
-        message: 'Please choose a date in the future',
-      })
-      startBtn.disabled = true;
-    } else {
-      UserSelectedDate = selectedDates[0];
-    
-      console.log(UserSelectedDate);
-      startBtn.disabled = false; 
-    }
-  },
-};
-flatpickr(userInput, options);
-
-
+// making elements to handle with listeners by html's names
 const elements = {
-  days: document.querySelector(".js-days"),
-  hours: document.querySelector(".js-hours"),
-  minutes: document.querySelector(".js-minutes"),
-  seconds: document.querySelector(".js-seconds")
+    input: document.querySelector("#datetime-picker"),
+    button: document.querySelector("button[data-start]"),
+    day: document.querySelector("span[data-days]"),
+    hour: document.querySelector("span[data-hours]"),
+    minute: document.querySelector("span[data-minutes]"),
+    second: document.querySelector("span[data-seconds]")
 }
+
+// making start button disabled when no data is chosen
+let userSelectedDate;
+elements.button.disabled = true;
+
+// options object for flatpickr plugin
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+// cooperate with izitoast plugin - pop-ups
+    onClose(selectedDates) {
+        const currentTime = Date.now();
+        if (selectedDates[0] <= currentTime) {
+            iziToast.error({
+                message: "Please choose a date in the future"
+            });
+            elements.button.disabled = true;
+        } else {
+            elements.button.disabled = false;
+            userSelectedDate = selectedDates[0];
+        }
+        return;
+    },
+};
+
+function runningCounter({ days, hours, minutes, seconds }) {
+    elements.day.textContent = `${days}`
+    elements.hour.textContent = `${hours}`
+    elements.minute.textContent = `${minutes}`
+    elements.second.textContent = `${seconds}`;
+}
+
+elements.button.addEventListener("click", handlerStart);
+
+function handlerStart() {
+    const newInterval = setInterval(() => {
+        const currentTime = Date.now();
+        const countdown = userSelectedDate - currentTime;
+        if (countdown <= 0) {
+            clearInterval(newInterval);
+            elements.input.disabled = false;
+            elements.button.disabled = false;
+            return;
+        }
+        const { days, hours, minutes, seconds } = convertMs(countdown);
+        runningCounter({ days, hours, minutes, seconds });
+        elements.input.disabled = true;
+        elements.button.disabled = true;
+    }, 1000);
+}
+
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    const days = addLeadingZero(Math.floor(ms / day));
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-  return { days, hours, minutes, seconds };
+    return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, "0")
 }
 
 
-startBtn.addEventListener('click', hadlerClick);
-function hadlerClick() {
-setInterval(() => {
-  if (UserSelectedDate) {
-    const currentTime = new Date();
-    const difference = UserSelectedDate - currentTime;
-  
-    if (difference <= 0) {
-      elements.days.textContent = '00';
-      elements.hours.textContent = '00';
-      elements.minutes.textContent = '00';
-      elements.seconds.textContent = '00';
-      return;
-    }
-
-    const { days, hours, minutes, seconds } = convertMs(difference);
-
-    elements.days.textContent = String(days).padStart(2,'0');
-    elements.hours.textContent = String(hours).padStart(2, '0');
-    elements.minutes.textContent = String(minutes).padStart(2, '0');
-    elements.seconds.textContent = String(seconds).padStart(2, '0');
-
-  }
-}, 1000)
-  startBtn.disabled = true; 
-  userInput.disabled = true;
-}
+flatpickr("#datetime-picker", options);
