e824fe82c0f90119a717e52c7ba2acb8a23566d3
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-07 16:10:49 +0200

    Update 02-snackbar.js

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -8,31 +8,5 @@ form.addEventListener('submit', handlerSubmit);
 function handlerSubmit(event) {
     event.preventDefault();
     
-    const delay = parseInt(evt.target.delay.value);
-    const radio = evt.target.state.value;
-
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (radio === "fulfilled") {
-                resolve(delay);
-            } else if (radio === "rejected") {
-                reject(delay);
-            }
-        }, delay);
-    });
-
-    promise
-        .then((delay) => {
-            iziToast.success({
-                
-                message: `✅ Fulfilled promise in ${delay}ms`,
-            });
-        })
-        .catch ((delay) => {
-        iziToast.error({
-            
-            message: `❌ Rejected promise in ${delay}ms`,
-        });
-    })
-    
-}
+    const delay = parseInt(event.target.delay.value);
+    const radio = event.target.state.value;
