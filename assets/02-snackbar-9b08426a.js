7a2783c16a2f0e5b2099252f52289bb6ce06323e
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-07 17:04:00 +0000

    Deploying to gh-pages from @ bartoszkucharuk/goit-js-hw-10@db3cbdb897c351a108db534502a5100a9b5cac26 🚀

--- assets/02-snackbar-9b08426a.js
+++
@@ -1,2 +0,0 @@
-import{i as r}from"./vendor-77e16229.js";const a=document.querySelector(".form");a.addEventListener("submit",m);function m(i){i.preventDefault();const t=parseInt(evt.target.delay.value),s=evt.target.state.value;new Promise((e,o)=>{setTimeout(()=>{s==="fulfilled"?e(t):s==="rejected"&&o(t)},t)}).then(e=>{r.success({message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{r.error({message:`❌ Rejected promise in ${e}ms`})})}
-//# sourceMappingURL=02-snackbar-9b08426a.js.map
